23a23ebeb630c33ffc45132623af958d9e511b64
sunny <>
2022-05-28 20:43:52 +0800

    样式调整

--- lib/pages/tabs/ToDo.dart
+++ lib/pages/tabs/ToDo.dart
@@ -32,10 +32,34 @@ class _ToDoState extends State<ToDo> {
 
   _toDo(){
     return lists.map((e) => Container(
-      child: Text(e['label']),
-      color: e['color'],
-      height: 60,
-      margin: EdgeInsets.all(8),
+      // child: Text(
+      //   e['label'],
+      //   style: TextStyle(
+      //     fontSize: 20,
+      //     fontWeight: FontWeight.w500,
+      //   ),
+      //   ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            e['label'],
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Text(e['describe'],
+          )
+        ],
+      ),
+      height: 80,
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
+      decoration: BoxDecoration(
+        color: e['color'],
+        borderRadius:BorderRadius.all(Radius.circular(8))
+      ),
     )).toList();
   }
   @override
