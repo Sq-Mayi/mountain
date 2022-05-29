0a3607b6f841e90617cce242d24656bcb90c40e9
sunny <>
2022-05-29 18:41:58 +0800

    保存界面

--- lib/pages/tabs/AddToDo.dart
+++ lib/pages/tabs/AddToDo.dart
@@ -9,18 +9,66 @@ class AddToDo extends StatefulWidget {
 
 class _AddToDoState extends State<AddToDo> {
   @override
+  var masterName = new TextEditingController();
+  var childName = new TextEditingController();
+  var reword = new TextEditingController();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    masterName.text = '';
+    childName.text = '';
+    reword.text = '';
+  }
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
-        children: [
-          IconButton(
-            onPressed: (){
 
-            }, 
-            icon:Icon(Icons.save))
-
-        ],
-      ),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "主任务名"
+              ),
+              controller: masterName,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "子任务名"
+              ),
+              controller: childName
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "奖励"
+              ),
+              controller: reword,
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: CupertinoButton(
+                onPressed: (){
+                  print(masterName);
+                  print(childName);
+                  print(reword);
+                },
+                child: Text(
+                  '保存',
+                  style: TextStyle(
+                    color: Colors.white
+                  ),
+                  ),
+                color: Colors.yellow,
+                ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
